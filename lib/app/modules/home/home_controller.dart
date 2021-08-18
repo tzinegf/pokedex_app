3ed31ef05a65996513641b970dad1d70f4b9620c
Tzinegf <eg_f1@hotmail.com>
2021-08-17 23:51:23 -0300

    Create pokemon model and filter controller

--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -1,10 +1,29 @@
 import 'package:get/get.dart';
+import 'package:pokedex_flutter_app/app/data/models/pokemon_model.dart';
 
 class HomeController extends GetxController {
+ 
+  RxList allPokemons = <Pokemon>[].obs;
+
+  RxList pokemonFilter = <Pokemon>[].obs;
 
   var currentPage = 0.obs;
 
   void setPage(int index) {
     currentPage.value = index;
   }
+
+void filterPokemon(){
+pokemonFilter.value = allPokemons.where((u) => (u.nome
+                            .toLowerCase()
+                            .contains(u.toLowerCase())))
+                        .toList() ;
 }
+
+
+
+
+}
+
+
+
